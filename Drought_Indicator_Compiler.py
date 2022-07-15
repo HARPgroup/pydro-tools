5a6f056d7e16985a1d7172dc09138e0c67f7d218
Kleiner <Joseph.Kleiner@deq.virginia.gov>
2022-07-15 15:38:36 -0400

    gw portion

--- Drought_Indicator_Compiler.py
+++ Drought_Indicator_Compiler.py
@@ -5,10 +5,27 @@ import pandas as pd
 def main():
 
     sw_df = get_data_vahydro(viewurl = 'streamflow-drought-timeseries-all-export')
-    print(sw_df.head())
+    # print(sw_df.head())
+
+    # reutrn only the 11 official drought evaluation region stream gage indicators
+    sw_official_df = sw_df[pd.notna(sw_df)['official_drought_region'] == True]
+
+    # return only those with a below normal drought status
+    sw_status_df = sw_official_df.query('`[nonex_pct]_propcode` > 0')
+    print(sw_status_df[['official_drought_region', '[nonex_pct]_propcode']])
+
+
+    gw_df = get_data_vahydro(viewurl = 'groundwater-drought-timeseries-all-export')
+    # print(gw_df.head())
+
+    # return only those with a below normal drought status
+    sw_status_df = gw_df.query('`[nonex_pct]_propcode` > 0')
+    print(sw_status_df[['Drought_Evaluation_Region', '[nonex_pct]_propcode']])
+    # print(sw_status_df.loc[sw_status_df['[nonex_pct]_propcode'].idxmax()])
+
 
     res_df = get_data_vahydro(viewurl = 'reservoir-drought-features-export')
-    print(res_df.head())
+    # print(res_df.head())
 
 
 def get_data_vahydro(viewurl, baseurl = "http://deq1.bse.vt.edu:81/d.dh"):
